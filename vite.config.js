da8e5b4d5fb756c58492d9ab7c3b7d12f9eaea29
misitebao <i@misitebao.com>
2022-06-23 22:04:55 +0800

    feat: use `fileURLToPath` instead of `path.resolve`

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
@@ -33,7 +33,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
